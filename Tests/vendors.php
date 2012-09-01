68b30b51981507fd9de901d7cf2378ec8ad4e9df
cedric-g <c.girard@lexik.fr>
2012-09-01 18:40:26 +0200

    run tests with sf 2.1 RC2

--- Tests/vendors.php
+++ Tests/vendors.php
@@ -25,9 +25,9 @@ $deps = array(
 
 $revs = array(
     'v2.1' => array(
-        'symfony'               => 'v2.1.0-RC1',
+        'symfony'               => 'v2.1.0-RC2',
         'doctrine-common'       => '2.2.3',
-        'doctrine-dbal'         => '2.2.3',
+        'doctrine-dbal'         => '2.2.2',
         'doctrine'              => '2.2.3',
         'doctrine-fixtures'     => 'origin/master',
         'mongodb'               => '1.0.0-BETA1',
