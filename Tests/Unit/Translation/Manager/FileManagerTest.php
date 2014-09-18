b137735ee6ec13589acdda055190448f402d4e03
David Herrmann <david@vworld.at>
2014-09-18 16:54:08 +0200

    Added more Propel feature tests and some bugfixes

--- Tests/Unit/Translation/Manager/FileManagerTest.php
+++ Tests/Unit/Translation/Manager/FileManagerTest.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\UnitOfWork as ORMUnitOfWork;
 
 use Lexik\Bundle\TranslationBundle\Manager\FileManager;
 use Lexik\Bundle\TranslationBundle\Tests\Unit\BaseUnitTestCase;
+use Lexik\Bundle\TranslationBundle\Propel\FileQuery;
 
 /**
  * Unit test for FileManager.
@@ -35,6 +36,17 @@ class FileManagerTest extends BaseUnitTestCase
      */
     private $odmStorage;
 
+    /**
+     *
+     * @var Lexik\Bundle\TranslationBundle\Storage\PropelStorage
+     */
+    private $propelStorage;
+
+    /**
+     * @var string
+     */
+    private $rootDir = '/test/root/dir/app';
+
     public function setUp()
     {
         $this->em = $this->getMockSqliteEntityManager();
@@ -48,6 +60,10 @@ class FileManagerTest extends BaseUnitTestCase
         $this->loadFixtures($this->dm);
 
         $this->odmStorage = $this->getMongoDBStorage($this->dm);
+
+        $con = $this->getMockPropelConnection();
+        $this->loadPropelFixtures($con);
+        $this->propelStorage = $this->getPropelStorage();
     }
 
     /**
@@ -55,7 +71,7 @@ class FileManagerTest extends BaseUnitTestCase
      */
     public function testORMCreate()
     {
-        $manager = new FileManager($this->ormStorage, '/test/root/dir/app');
+        $manager = new FileManager($this->ormStorage, $this->rootDir);
 
         $file = $manager->create('myDomain.en.yml', '/test/root/dir/src/Project/CoolBundle/Resources/translations');
         $this->assertEquals(ORMUnitOfWork::STATE_MANAGED, $this->em->getUnitOfWork()->getEntityState($file));
@@ -79,7 +95,7 @@ class FileManagerTest extends BaseUnitTestCase
      */
     public function testODMCreate()
     {
-        $manager = new FileManager($this->odmStorage, '/test/root/dir/app');
+        $manager = new FileManager($this->odmStorage, $this->rootDir);
 
         $file = $manager->create('myDomain.en.yml', '/test/root/dir/src/Project/CoolBundle/Resources/translations');
         $this->assertEquals(ODMUnitOfWork::STATE_MANAGED, $this->dm->getUnitOfWork()->getDocumentState($file));
@@ -99,12 +115,36 @@ class FileManagerTest extends BaseUnitTestCase
     }
 
     /**
+     * @group propel
+     */
+    public function testPropelCreate()
+    {
+        $manager = new FileManager($this->propelStorage, $this->rootDir);
+
+        $file = $manager->create('myDomain.en.yml', '/test/root/dir/src/Project/CoolBundle/Resources/translations');
+        $this->assertTrue($file->isNew());
+        $this->assertEquals('myDomain', $file->getDomain());
+        $this->assertEquals('en', $file->getLocale());
+        $this->assertEquals('yml', $file->getExtention());
+        $this->assertEquals('myDomain.en.yml', $file->getName());
+        $this->assertEquals('../src/Project/CoolBundle/Resources/translations', $file->getPath());
+
+        $file = $manager->create('messages.fr.xliff', '/test/root/dir/app/Resources/translations', true);
+        $this->assertFalse($file->isNew());
+        $this->assertEquals('messages', $file->getDomain());
+        $this->assertEquals('fr', $file->getLocale());
+        $this->assertEquals('xliff', $file->getExtention());
+        $this->assertEquals('messages.fr.xliff', $file->getName());
+        $this->assertEquals('Resources/translations', $file->getPath());
+    }
+
+    /**
      * @group orm
      */
     public function testORMGetFor()
     {
         $repository = $this->em->getRepository(self::ENTITY_FILE_CLASS);
-        $manager = new FileManager($this->ormStorage, '/test/root/dir/app');
+        $manager = new FileManager($this->ormStorage, $this->rootDir);
 
         $total = count($repository->findAll());
         $this->assertEquals(5, $total);
@@ -130,7 +170,7 @@ class FileManagerTest extends BaseUnitTestCase
     public function testODMGetFor()
     {
         $repository = $this->dm->getRepository(self::DOCUMENT_FILE_CLASS);
-        $manager = new FileManager($this->odmStorage, '/test/root/dir/app');
+        $manager = new FileManager($this->odmStorage, $this->rootDir);
 
         $total = count($repository->findAll());
         $this->assertEquals(5, $total);
@@ -147,6 +187,31 @@ class FileManagerTest extends BaseUnitTestCase
         $this->dm->flush();
 
         $total = count($repository->findAll());
+        $this->assertEquals(6, $total);
+    }
+
+    /**
+     * @group propel
+     */
+    public function testPropelGetFor()
+    {
+        $manager = new FileManager($this->propelStorage, $this->rootDir);
+
+        $total = FileQuery::create()->count();
+        $this->assertEquals(5, $total);
+
+        // get an existing file
+        $file = $manager->getFor('superTranslations.de.yml', '/test/root/dir/app/Resources/translations');
+        $file->save();
+
+        $total = FileQuery::create()->count();
+        $this->assertEquals(5, $total);
+
+        // get a new file
+        $file = $manager->getFor('superTranslations.it.yml', '/test/root/dir/app/Resources/translations');
+        $file->save();
+
+        $total = FileQuery::create()->count();
         $this->assertEquals(6, $total);
     }
 }
