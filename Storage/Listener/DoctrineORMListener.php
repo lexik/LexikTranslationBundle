c3045df619a1e39892fb89cf49bf20661106ae7b
Cédric Girard <cedricgd24@gmail.com>
2017-02-02 10:30:37 +0100

    Added listener to fix string length when utf8mb4 is used (#238)
    * added listener to fix string length when utf8mb4 is used
    
    * only update maaping for class in TranslationBundle namespace

---
+++ Storage/Listener/DoctrineORMListener.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Lexik\Bundle\TranslationBundle\Storage\Listener;
+
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+
+class DoctrineORMListener
+{
+    /**
+     * @param LoadClassMetadataEventArgs $eventArgs
+     */
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+    {
+        $params = $eventArgs->getEntityManager()->getConnection()->getParams();
+
+        if ('utf8mb4' !== strtolower($params['charset'])) {
+            return;
+        }
+
+        /** @var ClassMetadataInfo $metadata */
+        $metadata = $eventArgs->getClassMetadata();
+
+        if (false === strpos($metadata->getName(), 'TranslationBundle')) {
+            return;
+        }
+
+        foreach ($metadata->getFieldNames() as $name) {
+            $fieldMapping = $metadata->getFieldMapping($name);
+
+            if (isset($fieldMapping['type']) && 'string' === $fieldMapping['type']) {
+                $fieldMapping['length'] = 191;
+                $metadata->fieldMappings[$name] = $fieldMapping;
+            }
+        }
+    }
+}
