13ed9b9af2b4c0b23cf7d721dd3d15c7f3015867
Seb <sdieunidou@users.noreply.github.com>
2017-09-07 11:05:32 +0200

    Fix test of ImportTranslationsCommand

--- Tests/Command/ImportTranslationsCommandTest.php
+++ Tests/Command/ImportTranslationsCommandTest.php
@@ -98,7 +98,7 @@ class ImportTranslationsCommandTest extends WebTestCase
         $resultLines = explode("\n", $commandTester->getDisplay());
 
         $this->assertEquals('# LexikTranslationBundle:', $resultLines[0]);
-        $this->assertRegExp('/Using dir (.)+(lexik\/translation-bundle|lexik\/LexikTranslationBundle)\/Resources\/translations to lookup translation files/', $resultLines[1]);
+        $this->assertRegExp('/Using dir (.)+\/Resources\/translations to lookup translation files/', $resultLines[1]);
         $this->assertRegExp('/translations\/LexikTranslationBundle\.((fr)|(en))\.yml" \.\.\. 30 translations/', $resultLines[2]);
         $this->assertRegExp('/translations\/LexikTranslationBundle\.((fr)|(en))\.yml" \.\.\. 30 translations/', $resultLines[3]);
         $this->assertEquals('Removing translations cache files ...', $resultLines[4]);
