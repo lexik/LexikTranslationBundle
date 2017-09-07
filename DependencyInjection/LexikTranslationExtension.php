0299217f40b3152159d146d4146fbd31a571b77b
Seb <sdieunidou@users.noreply.github.com>
2017-09-07 09:59:16 +0200

    fix definition of storage service for sf 3.3 (#275)

--- DependencyInjection/LexikTranslationExtension.php
+++ DependencyInjection/LexikTranslationExtension.php
@@ -171,7 +171,7 @@ class LexikTranslationExtension extends Extension
         );
 
         $storageDefinition = new Definition();
-        $storageDefinition->setClass(new Parameter(sprintf('lexik_translation.%s.translation_storage.class', $storage)));
+        $storageDefinition->setClass($container->getParameter(sprintf('lexik_translation.%s.translation_storage.class', $storage)));
         $storageDefinition->setArguments($args);
 
         $container->setDefinition('lexik_translation.translation_storage', $storageDefinition);
