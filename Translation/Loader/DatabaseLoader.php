3adc6ab2902e032df8cb9fcfde1d8ad7a4dc6242
tuxes3 <nicolo@whatwedo.ch>
2022-02-03 11:24:31 -0500

    fixed Declaration

--- Translation/Loader/DatabaseLoader.php
+++ Translation/Loader/DatabaseLoader.php
@@ -31,7 +31,7 @@ class DatabaseLoader implements LoaderInterface
     /**
      * {@inheritdoc}
      */
-    public function load($resource, $locale, $domain = 'messages')
+    public function load(mixed $resource, string $locale, string $domain = 'messages'): MessageCatalogue
     {
         $catalogue = new MessageCatalogue($locale);
 
