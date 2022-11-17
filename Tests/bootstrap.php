b5e214c367b33cba59fe0307bc9b032514407cff
Bart McLeod <mcleod@spaceweb.nl>
2022-11-17 22:11:24 +0100

    Fix workflow 12

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
-
+/*
+ * The defaults are used in CI for the github action.
+ *
+ * Somehow copying a .env.ci there did not work, so I put defaults here that work
+ */
 define("ORM_TYPE", getenv("ORM") ?: "doctrine");
 
 define("DB_ENGINE", getenv("DB_ENGINE") ?: "pdo_mysql");
@@ -8,6 +12,7 @@ define("DB_PORT", getenv("DB_PORT") ?: 3306);
 define('DB_NAME', getenv("DB_NAME") ?: "lexik_translation_test");
 define("DB_USER", getenv("DB_USER") ?: "root");
 define("DB_PASSWD", getenv("DB_PASSWD") ?: "");
+define("MONGO_SERVER", getenv("MONGO_SERVER") ?: "mongodb://admin:secret@mongo:27017/admin");
 
 if (file_exists($file = __DIR__.'/autoload.php')) {
     require_once $file;
