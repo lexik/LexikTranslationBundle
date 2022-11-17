f24b225cd4450c430a93cb5b1ee9f599073417ec
Bart McLeod <mcleod@spaceweb.nl>
2022-11-17 21:31:39 +0100

    Fix workflow 9

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -3,11 +3,11 @@
 define("ORM_TYPE", getenv("ORM") ?: "doctrine");
 
 define("DB_ENGINE", getenv("DB_ENGINE") ?: "pdo_mysql");
-define("DB_HOST", getenv("DB_HOST") ?: "localhost");
-define("DB_PORT", getenv("DB_PORT") ?: null);
+define("DB_HOST", getenv("DB_HOST") ?: "127.0.0.1");
+define("DB_PORT", getenv("DB_PORT") ?: 3306);
 define('DB_NAME', getenv("DB_NAME") ?: "lexik_translation_test");
 define("DB_USER", getenv("DB_USER") ?: "root");
-define("DB_PASSWD", getenv("DB_PASSWD") ?: null);
+define("DB_PASSWD", getenv("DB_PASSWD") ?: "");
 
 if (file_exists($file = __DIR__.'/autoload.php')) {
     require_once $file;
