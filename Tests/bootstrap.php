f25be1352e04fb4ffd65b01aaa658e69d2c19e74
Bart McLeod <mcleod@spaceweb.nl>
2022-11-17 22:14:59 +0100

    Fix workflow 14

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -12,7 +12,7 @@ define("DB_PORT", getenv("DB_PORT") ?: 3306);
 define('DB_NAME', getenv("DB_NAME") ?: "lexik_translation_test");
 define("DB_USER", getenv("DB_USER") ?: "root");
 define("DB_PASSWD", getenv("DB_PASSWD") ?: "");
-define("MONGO_SERVER", getenv("MONGO_SERVER") ?: "mongodb://admin:secret@mongo:27017/admin");
+define("MONGO_SERVER", getenv("MONGO_SERVER") ?: "mongodb://admin:secret@127.0.0.1:27017/admin");
 
 if (file_exists($file = __DIR__.'/autoload.php')) {
     require_once $file;
